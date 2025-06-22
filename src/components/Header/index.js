e0b8e2c8ab983a5e8a327485e055ae77f47c642e
Oskar Podrucki <118886547+oskarpodrucki@users.noreply.github.com>
2025-06-22 16:46:42 +0200

    Removed unnecessary css

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -6,7 +6,7 @@ export default function Header() {
 	return (
 		<div className='flex justify-between items-center bg-black text-white h-[50px] px-4'>
 			<ThemeSwitcher className='flex-shrink-0' />
-			<h1 className='text-2xl text-center'>
+			<h1 className='text-2xl'>
 				Bussines News
 			</h1>
 		</div>
